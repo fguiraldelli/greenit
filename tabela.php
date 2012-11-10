20908ce1477f2e7888bbf5482a2e83ac64f24f0a
feafigueiredo <ferafigueiredo@gmail.com>
2012-11-10 20:51:12 -0200

    tabela

---
+++ tabela.php
@@ -0,0 +1,173 @@
+<?php
+
+echo "
+<head>
+        <meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"padrao.css\">
+</head>
+<body class=\"t\">
+<div class=\"geral\">
+    <table border=1 width=400>
+        <tr>
+            <td>
+            </td>
+            <td>
+                Houve aumento de faturamento e/ou mercado?
+            </td>
+            <td>
+                Houve redução de custos e ganhos de produtividade?
+            </td>
+            <td>
+                Houve fortalecimento da reputação da marca?
+            </td>
+            <td>
+                Houve promoção dos valores da companhia?
+            </td>
+            <td>
+                Houve gestão de conformidade e risco?
+            </td>
+            <td>
+                Houve melhoria das condições de acesso a capital?
+            </td>
+            <td>
+                Houve convergência com tendências tecnológicas?
+            </td>
+        </tr>
+                <tr>
+            <td>
+                Fortalecimento do engajamento com stakeholders
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Fortalecimento do engajamento com stakeholders
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Amplo escopo de impacto positivo (efeito guarda-chuva)
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Desenvolvimento socioeconômico local/regional
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Desenvolvimento e capacitação de recursos humanos
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Melhoria da eco-eficiência em processos e/ou produtos e serviços
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Resposta a necessidades e desafios críticos locais e regionais
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+        </tr>
+    </table>
+    </div>
+    </body>
+    ";
+?>
