f59c74e2b3a68132ea4131b24857a36ea7cc7ad9
feafigueiredo <ferafigueiredo@gmail.com>
2012-11-10 15:46:54 -0200

    index, css e pasta de imagens

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php ?>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="padrao.css">
+    </head>
+    <body>
+        <div class="geral">
+            <div class="cabecalho">
+                <div class="logo">
+                    <img class="logo" src="imgs/logo.jpg" />
+                </div>
+                <div class="titulo">
+                    Green IT UFSCar
+                </div>
+            </div>
+            <div class="menu">
+                MENUZINHO
+            </div>
+            <div class="conteudo">
+                CONTEUDO
+            </div>
+            <div class="rodape">
+                <?php //include("rodape.php");  ?>
+            </div>
+        </div
+    </body>
+</html>
