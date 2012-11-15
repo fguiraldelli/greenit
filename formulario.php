674f3dad4f1889b95a61819da2b46c6fd2ff19a2
fguira <fguira@fguira-Inspiron-N5010>
2012-11-15 17:46:47 -0200

    Feita a comunicação com o Banco de dados e testada com formulario.php

---
+++ formulario.php
@@ -0,0 +1,15 @@
+<?php
+include("connection.php");
+
+$q = $_GET['q'];
+if (($q == NULL) || ($q < 1) || ($q > 14))
+    header("location: formulario.php?q=1");
+$sql = "SELECT * FROM questoes where id = " . $q;
+$res = mysql_query($sql);
+$cont = 0;
+while($row = mysql_fetch_array($res)){
+    $cont++;
+    echo $row['id'] . " " . $row['questao'] . "<br>";
+}
+
+?>
