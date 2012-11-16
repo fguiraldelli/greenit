0b56f7472db80ebe3a6440cdaa6c8dd51199e51f
fguira <fguira@fguira-Inspiron-N5010>
2012-11-16 13:37:16 -0200

    Modificado css e tabela.php para tabela aparecer colorida.

--- tabela.php
+++ tabela.php
@@ -1,4 +1,5 @@
 <?php
+
 include("connection.php");
 //$sql = "SELECT * FROM questoes where id = " . $cont;
 $sql = "SELECT * FROM questoes";
@@ -16,7 +17,31 @@ $i = 0;
 while ($i < $linhas / 2) {
     echo "<tr><td>" . mysql_result($result, $i, 1) . "</td>";
     for ($j = 0; $j < $linhas / 2; $j++) {
-            print "<td> </td>";
+        switch ($j){
+                        case 0:
+                            print "<td class = s1> </td>";
+                            break;
+                        case 1:
+                            print "<td class = s2> </td>";
+                            break;
+                        case 2:
+                            print "<td class = s3> </td>";
+                            break;
+                        case 3:
+                            print "<td class = "."'s4'". "> </td>";
+                            break;
+                        case 4:
+                            print "<td class = "."'s5'". "> </td>";
+                            break;
+                        case 5:
+                            print "<td class = "."'s6'". "> </td>";
+                            break;
+                        case 6:
+                            print "<td class = "."'s7'". "> </td>";
+                            break;
+                        default : print "<td class = "."'s4'". "> </td>";
+                    }
+        //print "<td> </td>";
     }
     echo "</tr>";
     $i++;
