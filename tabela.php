c2e6475444c7f903f0ef777194eae8b65c241803
fguira <fguira@fguira-Inspiron-N5010>
2012-11-20 21:31:22 -0200

    TABELA COLORINDO!!!!!

--- tabela.php
+++ tabela.php
@@ -1,47 +1,35 @@
 <?php
+
 include ("sessao.php");
 include("connection.php");
+$data = date("Y-m-d");
 $sql = "SELECT * FROM questoes";
+$sql2 = "SELECT * FROM respostas WHERE idu = " . $_SESSION["idu"] . " AND data = '" . $data . "'";
+$result_user = mysql_query($sql2);
+$data_user = mysql_fetch_array($result_user);
 $result = mysql_query($sql);
 echo "<table class = resultado>";
 echo "<tr><td class=s0></td>";
 $linhas = mysql_num_rows($result);
+/*for ($k = 0; $k < $linhas; $k++) {
+    print "user" . mysql_result($result_user, $k, 0) . "<br>";
+    print "questao" . mysql_result($result_user, $k, 1) . "<br>";
+    print "resp" . mysql_result($result_user, $k, 2) . "<br>";
+}*/
 $i = 0;
-/*Imprime as perguntas da coluna*/
-while ($i < $linhas/2) {
+/* Imprime as perguntas da coluna */
+while ($i < $linhas / 2) {
     echo "<td class=s0>" . mysql_result($result, $i, 1) . "</td>";
     $i++;
 }
 echo "</tr>";
 $i = $linhas / 2;
-/*Imprime as perguntas da linha e colore a célula da tabela*/
+/* Imprime as perguntas da linha e colore a célula da tabela */
 while ($i < $linhas) {
     echo "<tr><td class=s0>" . mysql_result($result, $i, 1) . "</td>";
-    for ($j = 0; $j < $linhas/2; $j++) {
-        switch ($j){
-                        case 0:
-                            print "<td class = s1> </td>";
-                            break;
-                        case 1:
-                            print "<td class = s2> </td>";
-                            break;
-                        case 2:
-                            print "<td class = s3> </td>";
-                            break;
-                        case 3:
-                            print "<td class = "."'s4'". "> </td>";
-                            break;
-                        case 4:
-                            print "<td class = "."'s5'". "> </td>";
-                            break;
-                        case 5:
-                            print "<td class = "."'s6'". "> </td>";
-                            break;
-                        case 6:
-                            print "<td class = "."'s7'". "> </td>";
-                            break;
-                        default : print "<td class = "."'s4'". "> </td>";
-                    }
+    for ($j = 0; $j < $linhas / 2; $j++) {
+        print "<td class = s".  mysql_result($result_user, $i, 2) . 
+                mysql_result($result_user, $j, 2) ."> </td>";
         //print "<td> </td>";
     }
     echo "</tr>";
